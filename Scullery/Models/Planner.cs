f6b30bca97b6c33fcd4001d7f185247752bcf313
jjvega86 <jj.vega86@gmail.com>
2020-10-13 16:25:33 -0400

    Added Planner model to project

---
+++ Scullery/Models/Planner.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scullery.Models
+{
+    public class Planner
+    {
+        [Key]
+        public int PlannerId { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+        [ForeignKey("PodId")]
+        public int PodId { get; set; }
+
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string SpoonacularUserName { get; set; }
+        public string UserHash { get; set; }
+        
+
+    }
+}
