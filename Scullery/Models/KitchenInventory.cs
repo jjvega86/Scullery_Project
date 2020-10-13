c2f9028e95bb8220e0c553e2ca6bf0c7e39b4408
jjvega86 <jj.vega86@gmail.com>
2020-10-13 16:40:54 -0400

    Added KitchenInventory model to project

---
+++ Scullery/Models/KitchenInventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scullery.Models
+{
+    public class KitchenInventory
+    {
+        [Key]
+        public int KitchenInventoryId { get; set; }
+        
+        [ForeignKey("PodId")]
+        public int PodId { get; set; }
+        public Pod Pod { get; set; }
+    }
+}
