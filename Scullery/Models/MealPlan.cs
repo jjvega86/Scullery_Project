b147b4c00701a64afcf3170b99fa659a6b71631f
jjvega86 <jj.vega86@gmail.com>
2020-10-13 16:53:57 -0400

    Added MealPlan model to project

---
+++ Scullery/Models/MealPlan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scullery.Models
+{
+    public class MealPlan
+    {
+        [Key]
+        public int MealPlanId { get; set; }
+
+        [ForeignKey("PodId")]
+        public int PodId { get; set; }
+        public Pod Pod { get; set; }
+
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        
+
+
+
+    }
+}
