75530e2cf803ffd8ea21fc855e2a860d74417f2b
jjvega86 <jj.vega86@gmail.com>
2020-10-13 16:33:19 -0400

    Added SavedRecipe model to project

---
+++ Scullery/Models/SavedRecipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scullery.Models
+{
+    public class SavedRecipe
+    {
+        [Key]
+        public int SavedRecipeId { get; set; }
+
+        [ForeignKey("PlannerId")]
+        public string PlannerId { get; set; }
+        public Planner Planner { get; set; }
+        public int SpoonacularRecipeId { get; set; }
+        public string ImageURL { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeURL { get; set; }
+
+    }
+}
