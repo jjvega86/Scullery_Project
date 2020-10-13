85688bda0d872573787013685d918c15d0ccfc9d
jjvega86 <jj.vega86@gmail.com>
2020-10-13 16:49:23 -0400

    Added Ingredient model to project

---
+++ Scullery/Models/Ingredient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scullery.Models
+{
+    public class Ingredient
+    {
+        [Key]
+        public int IngredientId { get; set; }
+
+        [ForeignKey("KitchenInventoryId")]
+        public int KitchenInventoryId { get; set; }
+        public KitchenInventory KitchenInventory { get; set; }
+
+        public int SpoonacularIngredientId { get; set; }
+        public string IngredientName { get; set; }
+        public string UnitType { get; set; }
+        public double UnitQuantity { get; set; }
+        
+
+
+    }
+}
