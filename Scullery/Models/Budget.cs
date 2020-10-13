3bac877f1e9eacb0aeca9771e1ab8624f1d348d0
jjvega86 <jj.vega86@gmail.com>
2020-10-13 16:39:08 -0400

    Added Budget model to project

---
+++ Scullery/Models/Budget.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scullery.Models
+{
+    public class Budget
+    {
+        [Key]
+        public int BudgetId { get; set; }
+        
+        [ForeignKey("PodId")]
+        public int PodId { get; set; }
+        public Pod Pod { get; set; }
+
+        public double CurrentWeekBudget { get; set; }
+        public double CurrentWeekSpent { get; set; }
+        public DateTime? CurrentWeekStart { get; set; }
+        public DateTime? CurrentWeekEnd { get; set; }
+        public double CumulativeBudget { get; set; }
+        public double CumulativeSpent { get; set; }
+
+    }
+}
