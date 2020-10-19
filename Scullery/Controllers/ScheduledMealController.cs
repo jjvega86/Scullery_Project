38c23e38217246b1f2e50f2d44a5ae8751a8ea8b
jjvega86 <jj.vega86@gmail.com>
2020-10-19 12:14:53 -0400

    Added properties to MealEvent viewmodel

--- Scullery/Controllers/ScheduledMealController.cs
+++ Scullery/Controllers/ScheduledMealController.cs
@@ -1,20 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Scullery.Data;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Scullery.Services;
+using Scullery.Models.ViewModels;
+using Scullery.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Scullery.Controllers
 {
     public class ScheduledMealController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ScheduledMealController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
         // GET: ScheduledMealController
         public ActionResult Calendar()
         {
             return View();
         }
 
-      
+        public JsonResult GetEvents()
+        {
+            var scheduledMeals = _context.ScheduledMeals.ToList();
+            MealEvent event = new MealEvent();
+           
+            var events = dc.Events.ToList();
+            return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            
+        }
+
+
     }
 }
--- Scullery/Models/ViewModels/MealEvent.cs
+++ Scullery/Models/ViewModels/MealEvent.cs
@@ -7,6 +7,14 @@ namespace Scullery.Models.ViewModels
 {
     public class MealEvent
     {
+        public Int64 Id { get; set; }
 
+        public string Title { get; set; }
+
+        public string Start { get; set; }
+
+        public string End { get; set; }
+
+        public bool AllDay { get; set; }
     }
 }
