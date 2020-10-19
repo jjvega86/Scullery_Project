f8dc5cb74655ea2d4d42dbc9a9801155688416bc
jjvega86 <jj.vega86@gmail.com>
2020-10-19 10:36:18 -0400

    Removed logic from Calendar view

--- Scullery/Controllers/ScheduledMealController.cs
+++ Scullery/Controllers/ScheduledMealController.cs
@@ -10,78 +10,11 @@ namespace Scullery.Controllers
     public class ScheduledMealController : Controller
     {
         // GET: ScheduledMealController
-        public ActionResult Index()
+        public ActionResult Calendar()
         {
             return View();
         }
 
-        // GET: ScheduledMealController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: ScheduledMealController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ScheduledMealController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ScheduledMealController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ScheduledMealController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ScheduledMealController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ScheduledMealController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+      
     }
 }
