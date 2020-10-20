2915f8f390ce24dd97ab4c978ecf14cc2f77f218
jjvega86 <jj.vega86@gmail.com>
2020-10-20 13:56:25 -0400

    Added ShoppingListDates view model

---
+++ Scullery/Models/ViewModels/ShoppingListDates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scullery.Models.ViewModels
+{
+    public class ShoppingListDates
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
