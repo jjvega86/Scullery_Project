047b833f7915e161709c520a1601bc59c809d1a6
jjvega86 <jj.vega86@gmail.com>
2020-10-22 10:20:31 -0400

    Added methods to TimeTools to find first and last day of the week

--- Scullery/Utilities/TimeTools.cs
+++ Scullery/Utilities/TimeTools.cs
@@ -36,5 +36,21 @@ namespace Scullery.Utilities
                 yield return day;
         }
 
+        public static DateTime FirstDayOfWeek(this DateTime dt)
+        {
+            var culture = System.Threading.Thread.CurrentThread.CurrentCulture;
+            var diff = dt.DayOfWeek - culture.DateTimeFormat.FirstDayOfWeek;
+
+            if (diff < 0)
+            {
+                diff += 7;
+            }
+
+            return dt.AddDays(-diff).Date;
+        }
+
+        public static DateTime LastDayOfWeek(this DateTime dt) =>
+            dt.FirstDayOfWeek().AddDays(6);
+
     }
 }
