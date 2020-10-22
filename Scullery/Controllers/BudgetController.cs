8c7b7e6ef964dd1e6f8aae861c5d08904299ced8
jjvega86 <jj.vega86@gmail.com>
2020-10-22 09:24:44 -0400

    Added Budget link to navbar

--- Scullery/Controllers/BudgetController.cs
+++ Scullery/Controllers/BudgetController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Scullery.Data;
+using Scullery.Models;
+using Scullery.Models.ViewModels;
 using Scullery.Services;
 
 namespace Scullery.Controllers
@@ -20,8 +23,23 @@ namespace Scullery.Controllers
             _spoonacular = spoonacular;
 
         }
+        private string GetLoggedInUser()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return userId;
+        }
+
+        private Planner GetLoggedInPlanner()
+        {
+            var planner = _context.Planners.Where(c => c.IdentityUserId == GetLoggedInUser()).SingleOrDefault();
+            return planner;
+
+        }
         public IActionResult Index()
         {
+            var planner = GetLoggedInPlanner();
+            
             return View();
         }
     }
