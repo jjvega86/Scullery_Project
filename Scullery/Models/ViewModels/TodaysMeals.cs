82350124541ad16655af71c38a2e585d52a7253f
jjvega86 <jj.vega86@gmail.com>
2020-10-23 10:38:55 -0400

    Added properties to TodaysMeals viewmodel

--- Scullery/Models/ViewModels/TodaysMeals.cs
+++ Scullery/Models/ViewModels/TodaysMeals.cs
@@ -7,5 +7,23 @@ namespace Scullery.Models.ViewModels
 {
     public class TodaysMeals
     {
+        public string PlannerName { get; set; }
+
+        public List<MealCard> MealCards { get; set; }
+
+    }
+
+    public class MealCard
+    {
+        public string CookName { get; set; }
+
+        public string MealType { get; set; }
+
+        public string MealSlot { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public string RecipeUrl { get; set; }
+
     }
 }
